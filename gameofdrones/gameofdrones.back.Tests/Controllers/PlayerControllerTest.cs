da1e823b0664e31248169c7762bd0761fe377a01
dgonzalezgalvis <diegoandresgonzalez@hotmail.com>
2019-09-29 13:39:15 -0700

    Unit testing

---
+++ gameofdrones/gameofdrones.back.Tests/Controllers/PlayerControllerTest.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using gameofdrones.back.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using gameofdrones.back.Models;
+
+namespace gameofdrones.back.Tests.Controllers
+{
+    [TestClass]
+    class PlayerControllerTest
+    {
+        [TestMethod]
+        public void Get()
+        {
+            // Arrange
+            PlayersController controller = new PlayersController();
+
+            // Act
+            IEnumerable<Player> result = controller.GetPlayers();
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(2, result.Count());
+            Assert.AreEqual("value1", result.ElementAt(0));
+            Assert.AreEqual("value2", result.ElementAt(1));
+        }
+
+        [TestMethod]
+        public void GetById()
+        {
+            // Arrange
+            ValuesController controller = new ValuesController();
+
+            // Act
+            string result = controller.Get(5);
+
+            // Assert
+            Assert.AreEqual("value", result);
+        }
+
+        [TestMethod]
+        public void Post()
+        {
+            // Arrange
+            ValuesController controller = new ValuesController();
+
+            // Act
+            controller.Post("value");
+
+            // Assert
+        }
+
+        [TestMethod]
+        public void Put()
+        {
+            // Arrange
+            ValuesController controller = new ValuesController();
+
+            // Act
+            controller.Put(5, "value");
+
+            // Assert
+        }
+
+        [TestMethod]
+        public void Delete()
+        {
+            // Arrange
+            ValuesController controller = new ValuesController();
+
+            // Act
+            controller.Delete(5);
+
+            // Assert
+        }
+    }
+}
